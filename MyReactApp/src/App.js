2fae98f87705b8f32065b6ed0e76b1cc01a5bc47
Ajinkya Ghodekar <ajinkyasghodekar@gmail.com>
2026-01-26 11:34:25 +0530

    changes

--- MyReactApp/src/App.js
+++ MyReactApp/src/App.js
@@ -1,4 +1,4 @@
-﻿import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
